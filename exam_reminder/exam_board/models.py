1ac8d69d92669236e5c5107e9f30adc5b2eea132
Dawid <MDDawid0323@gmail.com>
2022-12-11 20:26:09 +0100

    create "Exam" model

--- exam_reminder/exam_board/models.py
+++ exam_reminder/exam_board/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+
+class Exam(models.Model):
+    name = models.CharField(max_length=200)
+    subject = models.CharField(max_length=200)
+    room = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    description = models.CharField(max_length=2000, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ExamFrom(forms.Form):
+    class Meta:
+        model = Exam
+        fields = ('name', 'subject', 'room', 'date', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super(ExamFrom, self).__init__(*args, **kwargs)
+        self.fields['description'].required = False
