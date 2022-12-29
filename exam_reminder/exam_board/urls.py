fa1753330c48e622ff7de058d1c5a191a3c9a313
Dawid <MDDawid0323@gmail.com>
2022-12-29 23:01:51 +0100

    add past exams
    added ability to preview past exams
    fixed time zone related error

--- exam_reminder/exam_board/urls.py
+++ exam_reminder/exam_board/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.board, name="board"),
-    path('<int:exam_id>', views.exam, name="exam")
+    path('<int:exam_id>', views.exam, name="exam"),
+    path('past', views.past_board, name="past_board")
 ]
--- exam_reminder/exam_board/views.py
+++ exam_reminder/exam_board/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Exam
 from .forms import CreateExam
-import datetime
+from django.utils.timezone import now
 
 
 def board(response):
-    exams = Exam.objects.all().filter(date__gte=datetime.date.today()).order_by('date')
+    exams = Exam.objects.all().filter(date__gte=now()).order_by('date')
+    return render(response, "exam_board/board.html", {'exams': exams})
+
+
+def past_board(response):
+    exams = Exam.objects.all().filter(date__lt=now()).order_by('-date')
     return render(response, "exam_board/board.html", {'exams': exams})
 
 
