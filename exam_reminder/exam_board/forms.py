f8b9dd0027f69ed1a949577a4abb61f02021af5a
Dawid <MDDawid0323@gmail.com>
2023-03-12 18:12:48 +0100

    make forms work, enable editing of objects

--- exam_reminder/exam_board/forms.py
+++ exam_reminder/exam_board/forms.py
@@ -7,10 +7,17 @@ from .models import Exam
 #         model = Exam
 #         fields = ('name', 'subject', 'room', 'date', 'description')
 
-class CreateExam(forms.Form):
-    name = forms.CharField(max_length=200)
-    subject = forms.CharField(max_length=200)
-    room = forms.CharField(max_length=200)
-    date = forms.DateTimeField()
-    description = forms.CharField(max_length=2000, required=False)
+class CreateExam(forms.ModelForm):
+    class Meta:
+        model = Exam
+        fields = '__all__'
+
+        widgets = {
+            'date': forms.TextInput(attrs={'type': 'datetime-local'}),
+        }
+    # name = forms.CharField(max_length=200)
+    # subject = forms.CharField(max_length=200)
+    # room = forms.CharField(max_length=200)
+    # date = forms.DateTimeField()
+    # description = forms.CharField(max_length=2000, required=False)
     
--- exam_reminder/exam_board/views.py
+++ exam_reminder/exam_board/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Exam
 from .forms import CreateExam
 from django.utils.timezone import now
 
 
 def board(response):
+    if response.method == "POST":
+        form = CreateExam(response.POST)
+        if form.is_valid():
+            e = form.save()
+            return redirect('exam', e.id)
+
     exams = Exam.objects.all().filter(date__gte=now()).order_by('date')
-    return render(response, "exam_board/board.html", {'exams': exams})
+    f = CreateExam()
+    return render(response, "exam_board/board.html", {'exams': exams, 'form': f})
 
 
 def past_board(response):
@@ -16,5 +23,10 @@ def past_board(response):
 
 def exam(response, exam_id):
     e = get_object_or_404(Exam, id=exam_id)
-    f = CreateExam()
+    if response.method == "POST":
+        form = CreateExam(response.POST, instance=e)
+        if form.is_valid():
+            form.save()
+
+    f = CreateExam(initial=e.__dict__)
     return render(response, 'exam_board/exam.html', {'exam': e, 'form': f})
