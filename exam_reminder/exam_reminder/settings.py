5f7d2e7d3a51faccef80bc49cfbc9df2a18d9568
Dawid <MDDawid0323@gmail.com>
2023-03-12 22:44:25 +0100

    Update settings.py

--- exam_reminder/exam_reminder/settings.py
+++ exam_reminder/exam_reminder/settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'exam_reminder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'exam_reminder_db',
-        'USER': 'postgres',
-        'PASSWORD': environ['DB_PASSWORD'],
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'exam_reminder_db',
+#         'USER': 'postgres',
+#         'PASSWORD': environ['DB_PASSWORD'],
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
